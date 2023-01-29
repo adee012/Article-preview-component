4fe23aee72589b9665380f7867af1dea0b1a1b5a
ddwptr_ <adedwiputra16@gmail.com>
2023-01-29 16:02:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const share = document.querySelector(".share")
+const link = document.querySelector(".share-link")
+const card = document.querySelector(".card")
+
+share.addEventListener("click", function(){ 
+    link.classList.remove("hide");
+   })
+
